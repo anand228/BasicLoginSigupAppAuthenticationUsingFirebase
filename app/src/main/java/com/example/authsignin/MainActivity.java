386a1912324043aeff23f9f3569726dc943cf27a
anand228 <anandsrivastav228@gmail.com>
2021-06-07 01:44:28 +0530

    Signup task complete

--- app/src/main/java/com/example/authsignin/MainActivity.java
+++ app/src/main/java/com/example/authsignin/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.authsignin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText signupPassword;
+    private EditText signupEmail;
+    private Button signupButton;
+    private EditText signupName;
+    private FirebaseAuth mAuth;
+    private  String  getEmailSignup;
+    private  String getPasswordSignup;
+    private  String getNameSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        setView();
+    }
+
+    private  void  setView(){
+        signupEmail = findViewById(R.id.signupTextEmailAddress);
+        signupPassword = findViewById(R.id.signupTextPassword);
+        signupButton = findViewById(R.id.signupButton);
+        signupName = findViewById(R.id.signupTextPersonName);
+    }
+
+    private void OnClickListener(){
+        signupButton.setOnClickListener(view -> {
+            getEmailSignup = signupEmail.getText().toString().trim();
+            getPasswordSignup = signupPassword.getText().toString().trim();
+            add_user();
+        });
+    }
+
+    private void add_user(){
+        mAuth.createUserWithEmailAndPassword(getEmailSignup, getPasswordSignup)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(this, "signup unsuccessful", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
